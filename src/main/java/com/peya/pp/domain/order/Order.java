e0672909634b4b2e233765db9a6f61ba8c28f4fb
Martin Morales <martinm@vandy.ai>
2023-11-09 00:52:57 -0300

    - Updated to add more domain-friendly example

--- src/main/java/com/peya/pp/domain/order/Order.java
+++ src/main/java/com/peya/pp/domain/order/Order.java
@@ -3,6 +3,9 @@ package com.peya.pp.domain.order;
 
 import java.util.UUID;
 
+/**
+ * Order created for the user that represents a request for an order
+ */
 public class Order {
     private UUID id;
 
